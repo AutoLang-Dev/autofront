05ac262483706278bf01dc3e9946b9a19e1a9123
Teru Shigure <teru@autolang.dev>
2026-01-17 05:38:22 +0800

    refactor: remove useless macros

--- macros/src/names.rs
+++ macros/src/names.rs
@@ -4,31 +4,17 @@ use proc_macro2::TokenStream;
 use quote::quote;
 
 pub struct Names {
-   pub Parse: TokenStream,
-   pub ParseBuffer: TokenStream,
-   pub Result: TokenStream,
-   pub ParseError: TokenStream,
-   pub DiagSink: TokenStream,
    pub AstPrint: TokenStream,
    pub Spanned: TokenStream,
    pub Span: TokenStream,
-   pub Error: TokenStream,
 }
 
 impl Names {
    pub fn new() -> Self {
-      let ns = quote! { crate::parser };
-
       Self {
-         Parse: quote! { #ns::syntax::parse::Parse },
-         ParseBuffer: quote! { #ns::buffer::ParseBuffer },
-         Result: quote! { #ns::syntax::parse::Result },
-         ParseError: quote! { #ns::syntax::parse::ParseError },
-         DiagSink: quote! { diag::DiagSink },
          AstPrint: quote! { crate::print::AstPrint },
          Spanned: quote! { crate::span::Spanned },
          Span: quote! { common::span::Span },
-         Error: quote! { #ns::syntax::token::Error },
       }
    }
 }
